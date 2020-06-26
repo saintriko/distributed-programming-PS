ce1153a8db7809bd749403bc1cdd9620c97866db
Riko <makssemeev@gmail.com>
2020-06-27 00:31:21 +0300

    Update Program.cs

--- 3lw/src/JobLogger/Program.cs
+++ 3lw/src/JobLogger/Program.cs
@@ -10,10 +10,15 @@ namespace JobLogger
         {
            var loggerservice = new LoggerService();
 
-
+            try {
             using(IConnection connection = new ConnectionFactory().CreateConnection(ConnectionFactory.GetDefaultOptions())){
                 loggerservice.Run(connection);
                 Console.WriteLine("JobLogger online");
+                Console.ReadKey();
+            }
+            } catch(NATS.Client.NATSNoServersException ex) {
+                Console.WriteLine("No connection to NATS");
+                Console.WriteLine(ex.Message);
                 Console.ReadKey();
             }
         }
