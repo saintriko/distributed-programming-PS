9ce44bb07d5fbc1d7941aab25e13c1b18605c1b5
Riko <makssemeev@gmail.com>
2020-06-27 14:18:22 +0300

    Update LoggerService.cs

--- 3lw/src/JobLogger/LoggerService.cs
+++ 3lw/src/JobLogger/LoggerService.cs
@@ -15,7 +15,7 @@ namespace Subscriber
         
         private IConfigurationRoot config;
 
-        public string getRedisValue(string id)
+        public string getDescriptionInDB(string id)
         {
             var config = new ConfigurationBuilder()  
                         .SetBasePath(new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.FullName + "/config")  
@@ -26,9 +26,9 @@ namespace Subscriber
             return db.StringGet(id);
         }
 
-        public void WriteLog(string id)
+        public void logToConsole(string id)
         {
-            string redisValue = getRedisValue(id);
+            string redisValue = getDescriptionInDB(id);
             Console.WriteLine(id + " : " + redisValue);
         }
 
@@ -37,7 +37,7 @@ namespace Subscriber
             var greetings = connection.Observe("events")
                     .Select(m => Encoding.Default.GetString(m.Data));
 
-            greetings.Subscribe(msg => WriteLog(msg));
+            greetings.Subscribe(msg => logToConsole(msg));
         }    
     }
 }
